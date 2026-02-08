1d1883bee68a40c4fa7153bf5b012230fd453062
Bunlung777 <napat2559@hotmail.com>
2026-02-08 16:50:23 +0700

    3 commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { exp } from "firebase/firestore/pipelines";
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { exp } from "firebase/firestore/pipelines";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
